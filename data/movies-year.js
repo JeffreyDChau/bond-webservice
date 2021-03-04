c486bd2bf4b5b50fb7f6281145a540e2019aa8ff
Jeffrey Chau <jeffreyd.chau@gmail.com>
2021-03-03 21:30:12 -0800

    year

---
+++ data/movies-year.js
@@ -0,0 +1,89 @@
+{
+	"title": "Movies By Year",
+	"films":[
+			   {
+				"Title":"Jaws",
+				"Rank":4,
+				"Grossing": "470.7 million Dollars",
+				"Director":"Steven Spielberg",
+				"Year":1975,
+				"Image":"jaws.jpg"
+			  },
+			  {
+				"Title":"Indiana Jones Lost Ark",
+				"Rank":8,
+				"Grossing": "212.2 million Dollars",
+				"Director":"Steven Spielberg",
+				"Year":1981,
+				"Image":"jones_ark.jpg"
+			  },
+			  {
+				"Title":"Indiana Jones Temple of Doom",
+				"Rank":6,
+				"Grossing": "179.8 million Dollars",
+				"Director":"Steven Spielberg",
+				"Year":1993,
+				"Image":"jones_temple.jpg"
+			  },
+			  {
+				"Title":"Back to the Future 1",
+				"Rank":2,
+				"Grossing": "381.1 million Dollars",
+				"Director":"Robert Zemeckis",
+				"Year":1985,
+				"Image":"future1.jfif"
+			  },
+			  {
+				"Title":"Back to the Future 2",
+				"Rank":1,
+				"Grossing": "331.9 million Dollars",
+				"Director":"Robert Zemeckis",
+				"Year":1989,
+				"Image":"future2.jpg"
+			  },
+			  {
+				"Title":"Indiana Jones Last Crusade",
+				"Rank":9,
+				"Grossing": "197.2 million Dollars",
+				"Director":"Steven Spielberg",
+				"Year":1989,
+				"Image":"jones_crusade.png"
+			  },
+			  {
+				"Title":"Back to the Future 3",
+				"Rank":3,
+				"Grossing": "244.0 million Dollars",
+				"Director":"Robert Zemeckis",
+				"Year":1990,
+				"Image":"future3.jpg"
+			  },
+			  {
+				"Title":"Jurassic Park",
+				"Rank":5,
+				"Grossing": "652.27 million Dollars",
+				"Director":"Steven Spielberg",
+				"Year":1993,
+				"Image":"park.jpg"
+			  },
+			  {
+				"Title":"Indiana Jones Crystal Skull",
+				"Rank":10,
+				"Grossing": "317.1 million Dollars",
+				"Director":"Steven Spielberg",
+				"Year":2008,
+				"Image":"jones_skull.jpg"
+			  },
+			  {
+				"Title":"Ready Player 1",
+				"Rank":7,
+				"Grossing": "582.9 million Dollars",
+				"Director":"Steven Spielberg",
+				"Year":2018,
+				"Image":"player1.jpg"
+			  }
+			  
+			  
+			  
+			  
+			]
+}
