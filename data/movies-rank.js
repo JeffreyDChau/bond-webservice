319ae4b026cbb4e49a8d074f44e4b4af53d0ba63
Jeffrey Chau <jeffreyd.chau@gmail.com>
2021-03-03 17:07:55 -0800

    movie rank

---
+++ data/movies-rank.js
@@ -0,0 +1,86 @@
+{
+	"title": "Movies By Personal Rank",
+	"movies":[
+			  {
+				"Title":"Back to the Future 2",
+				"Personal Ranking":1,
+				"Grossing": "331.9 million Dollars",
+				"Director":"Robert Zemeckis",
+				"Year":1989,
+				"Image":"future2.jpg"
+			  },
+			  {
+				"Title":"Back to the Future 1",
+				"Personal Ranking":2,
+				"Grossing": "381.1 million Dollars",
+				"Director":"Robert Zemeckis",
+				"Year":1985,
+				"Image":"future1.jfif"
+			  },
+			  {
+				"Title":"Back to the Future 3",
+				"Personal Ranking":3,
+				"Grossing": "244.0 million Dollars",
+				"Director":"Robert Zemeckis",
+				"Year":1990,
+				"Image":"future3.jpg"
+			  },
+			  {
+				"Title":"Jaws",
+				"Personal Ranking":4,
+				"Grossing": "470.7 million Dollars",
+				"Director":"Steven Spielberg",
+				"Year":1975,
+				"Image":"jaws.jpg"
+			  },
+			  {
+				"Title":"Jurassic Park",
+				"Personal Ranking":5,
+				"Grossing": "652.27 million Dollars",
+				"Director":"Steven Spielberg",
+				"Year":1993,
+				"Image":"park.jpg"
+			  },
+			  {
+				"Title":"Indiana Jones Temple of Doom",
+				"Personal Ranking":6,
+				"Grossing": "179.8 million Dollars",
+				"Director":"Steven Spielberg",
+				"Year":1993,
+				"Image":"jones_temple.jpg"
+			  },
+			  {
+				"Title":"Ready Player 1",
+				"Personal Ranking":7,
+				"Grossing": "582.9 million Dollars",
+				"Director":"Steven Spielberg",
+				"Year":2018,
+				"Image":"player1.jpg"
+			  },
+			  {
+				"Title":"Indiana Jones Lost Ark",
+				"Personal Ranking":8,
+				"Grossing": "212.2 million Dollars",
+				"Director":"Steven Spielberg",
+				"Year":1981,
+				"Image":"jones_ark.jpg"
+			  },
+			  {
+				"Title":"Indiana Jones Last Crusade",
+				"Personal Ranking":9,
+				"Grossing": "197.2 million Dollars",
+				"Director":"Steven Spielberg",
+				"Year":1989,
+				"Image":"jones_crusade.png"
+			  },
+			  {
+				"Title":"Indiana Jones Crystal Skull",
+				"Personal Ranking":10,
+				"Grossing": "317.1 million Dollars",
+				"Director":"Steven Spielberg",
+				"Year":2008,
+				"Image":"jones_skull.jpg"
+			  }
+			  
+			]
+}
---
+++ data/restaurant-rating.js
@@ -0,0 +1,13 @@
+{
+	"title": "Restaurant by Rating",
+	"restaurants":[
+			  {
+				"Name":"Applebees",
+				"Title":"206 266 8828",
+				"Address": "123 sdsd wdw",
+				"Rating":5,
+				"Cuisine":"Burgers",
+			  },
+			  
+			]
+}
