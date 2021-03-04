e8fb46cc2c27cf11a2c1e284fba42cba70ce792c
Jeffrey Chau <jeffreyd.chau@gmail.com>
2021-03-03 21:43:03 -0800

    edits

--- data/restaurant-rating.js
+++ data/restaurant-rating.js
@@ -2,11 +2,17 @@
 	"title": "Restaurant by Rating",
 	"restaurants":[
 			  {
-				"Name":"Applebees",
-				"Title":"206 266 8828",
-				"Address": "123 sdsd wdw",
-				"Rating":5,
-				"Cuisine":"Burgers",
+				"Film":1,
+				"Title":"Dr. No",
+				"Year":1962,
+				"Director":"Terence Young",
+				"Producers":"Harry Saltzman and Albert R. Broccoli",
+				"Writers":"Richard Maibaum, Johanna Harwood and Berkely Mather",
+				"Composer":"Monty Norman",
+				"Bond":"Sean Connery",
+				"Budget":"$1,000,000.00",
+				"BoxOffice":"$59,567,035.00",
+				"Image":"dr-no.jpg"
 			  },
 			  
 			]
